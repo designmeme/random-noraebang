8f8facedfd538032182e76c06525d8ac65f9c86d
jihye <ghe.lee19@gmail.com>
2020-03-08 17:00:13 +0900

    15 favorite delete

--- src/app/modules/favorites/favorites.page.ts
+++ src/app/modules/favorites/favorites.page.ts
@@ -5,6 +5,7 @@ import {AuthService} from '../../core/services/auth.service';
 import {catchError, finalize, map, switchMap, tap} from 'rxjs/operators';
 import {AngularFireDatabase} from '@angular/fire/database';
 import {FavoritesApiService} from '../../core/http/favorites-api.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-favorites',
@@ -15,9 +16,12 @@ export class FavoritesPage implements OnInit {
   list$: Observable<Favorite[]>;
   error: any;
 
+  deleteLoading = false;
+
   constructor(
     public authService: AuthService,
     private favoritesApi: FavoritesApiService,
+    private alertController: AlertController,
   ) { }
 
   ngOnInit() {
@@ -34,6 +38,29 @@ export class FavoritesPage implements OnInit {
   }
 
   delete(item: Favorite) {
-    alert('delete');
+    if (this.deleteLoading) {
+      return;
+    }
+
+    this.alertController.create({
+      header: '즐겨찾기 삭제',
+      message: '즐겨찾기에서 삭제할까요?',
+      buttons: [
+        {
+          text: '아니오',
+          role: 'cancel',
+        }, {
+          text: '예',
+          handler: () => {
+            this.deleteLoading = true;
+            this.favoritesApi.deleteItem(item.songId).pipe(
+              tap(() => this.deleteLoading = false),
+              finalize(() => this.deleteLoading = false),
+            ).subscribe();
+          }
+        }
+      ]
+    }).then(alert => alert.present());
+
   }
 }
