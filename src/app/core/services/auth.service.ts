8f885512256c47f38eb574afa13a2b5111f148b9
jihye <ghe.lee19@gmail.com>
2020-03-10 23:41:31 +0900

    fix(auth): 브라우저 닫혀도 로그인 유지하도록 설정

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {auth as fireAuth} from 'firebase/app';
-import {map} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +23,16 @@ export class AuthService {
   }
 
   async login() {
+    try {
+      // https://firebase.google.com/docs/auth/web/auth-state-persistence#overview_of_persistence_behavior
+      // 로그인 유지하기 설정
+      await this.auth.auth.setPersistence(fireAuth.Auth.Persistence.LOCAL);
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorCode + errorMessage);
+    }
+
     return await this.auth.auth.signInWithPopup(new fireAuth.GoogleAuthProvider());
   }
 
